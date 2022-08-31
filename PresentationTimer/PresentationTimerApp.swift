1d77f6dfc34b4b2734f548af42289243ca5b471d
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-01 01:12:17 +0900

    Initial Commit

---
+++ PresentationTimer/PresentationTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationTimerApp.swift
+//  PresentationTimer
+//
+//  Created by iwashita on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct PresentationTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
